8613a35c7debc9dab1da3e1bce31710208b130fd
Renato <nato.rjf@gmail.com>
2018-03-13 17:46:00 -0300

    - Installing some pakages and configuring the webpack

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const a = 3;
-const b = 1;
-const c = '1';
-
-function sumValues() {
-  return a + b + c;
-}
-
-sumValues();
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
