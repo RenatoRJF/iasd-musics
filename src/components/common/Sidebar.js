0bdf4e85805143bb2bfb26d7fb1c90f6a0facee0
Renato <nato.rjf@gmail.com>
2018-03-15 13:25:18 -0300

    - Customizing the Sideber's menu - Updating the icons

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -2,7 +2,45 @@ import React from 'react';
 
 const Sidebar = () => (
   <aside className="app-main__sidebar">
-    aside
+    <nav className="sidebar-nav">
+      <ul>
+        <li className="sidebar-menu__group">
+          <a href="/" className="sidebar-menu__group-title active-menu">
+            <i className="icon-file-music" /> Ministério Jovem
+          </a>
+          <ul className="menu__group-list">
+            <li>
+              <a href="/">
+                <i className="icon-cd" /> 2015 - Eu sou a mensagem
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="icon-cd" /> 2015 - Eu sou a mensagem
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li className="sidebar-menu__group active-menu-group">
+          <a href="/" className="sidebar-menu__group-title">
+            <i className="icon-file-music" /> Ministério Jovem
+          </a>
+          <ul className="menu__group-list">
+            <li>
+              <a href="/" className="active-menu-item">
+                <i className="icon-cd" /> 2015 - Eu sou a mensagem
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="icon-cd" /> 2015 - Eu sou a mensagem
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
   </aside>
 );
 
